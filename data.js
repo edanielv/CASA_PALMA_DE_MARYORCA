518832a43030f5d53a510129ceee5e6d3bb55af0
edanielv <edanielvaron.arq@gmail.com>
2026-06-17 21:13:52 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,351 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-00-entrada",
+      "name": "00-ENTRADA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.04794741930400903,
+        "pitch": -0.037839576567828104,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.06116164751939834,
+          "pitch": 0.19331701323098294,
+          "rotation": 0,
+          "target": "1-01-sala"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-01-sala",
+      "name": "01-SALA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1105761392276765,
+          "pitch": 0.019860512892513782,
+          "rotation": 0,
+          "target": "2-02-piscina"
+        },
+        {
+          "yaw": -2.8967823214380637,
+          "pitch": 0.10131694402611124,
+          "rotation": 0,
+          "target": "0-00-entrada"
+        },
+        {
+          "yaw": 1.7152815658235054,
+          "pitch": -0.056589759605149226,
+          "rotation": 0.7853981633974483,
+          "target": "3-03-pasillo-nivel-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-02-piscina",
+      "name": "02-PISCINA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.24375951050871691,
+          "pitch": 0.053821238432657736,
+          "rotation": 0,
+          "target": "1-01-sala"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-03-pasillo-nivel-02",
+      "name": "03-PASILLO NIVEL 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.499312868992291,
+          "pitch": 0.5131871050713759,
+          "rotation": 8.63937979737193,
+          "target": "1-01-sala"
+        },
+        {
+          "yaw": -2.911305656653685,
+          "pitch": 0.08150185954900735,
+          "rotation": 0.7853981633974483,
+          "target": "7-07-habitacin-03"
+        },
+        {
+          "yaw": 0.428314188975504,
+          "pitch": 0.22194614047295858,
+          "rotation": 0.7853981633974483,
+          "target": "4-04-bao"
+        },
+        {
+          "yaw": 0.03149169629702975,
+          "pitch": 0.18432167724516368,
+          "rotation": 0,
+          "target": "5-05-habitacin-01"
+        },
+        {
+          "yaw": -0.7665952870415857,
+          "pitch": 0.3972149120950892,
+          "rotation": 4.71238898038469,
+          "target": "6-06-habitacin-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-04-bao",
+      "name": "04-BAÑO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.15842219364256138,
+        "pitch": 0.35621131658923844,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8732501263308237,
+          "pitch": 0.5418867624045589,
+          "rotation": 0,
+          "target": "3-03-pasillo-nivel-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-05-habitacin-01",
+      "name": "05-HABITACIÓN-01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.696414992531442,
+          "pitch": 0.3471872238738829,
+          "rotation": 4.71238898038469,
+          "target": "3-03-pasillo-nivel-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-06-habitacin-02",
+      "name": "06-HABITACIÓN-02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.298446365919398,
+          "pitch": 0.2209111727222517,
+          "rotation": 4.71238898038469,
+          "target": "3-03-pasillo-nivel-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-07-habitacin-03",
+      "name": "07-HABITACIÓN-03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0456660987580655,
+          "pitch": 0.0718376884942522,
+          "rotation": 1.5707963267948966,
+          "target": "3-03-pasillo-nivel-02"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "apto_palmas_de_mallorca",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
